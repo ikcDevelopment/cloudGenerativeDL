472be00753f9c554614c01798d95f86ae7d3971f
ikcDevelopment <estuardo.wyss@yahoo.com>
2024-10-02 15:20:59 -0600

    first commit

---
+++ src/main/java/wyss/generative/ms/learning/HelloApplication.java
@@ -0,0 +1,9 @@
+package wyss.generative.ms.learning;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
